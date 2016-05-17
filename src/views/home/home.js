41e5c8b44e581d385b64712f6ae67adb34b6171b
Vivek Maharajh <vmaha@ntdev.corp.microsoft.com>
2016-05-16 22:10:57 -0700

    finished selling points

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,8 +1,8 @@
 ﻿controllers.controller('home', ['$scope',
     function($scope) {
         
-        function QuotationSellingPoint(type, callToAction, quote, attribution, attributionDescription) {
-            this.type = type;
+        function QuotationSellingPoint(callToAction, quote, attribution, attributionDescription) {
+            this.type = 'quotation';
             this.callToAction = callToAction;
             
             this.quote = {
@@ -14,22 +14,40 @@
             };
         }
         
+        function ChecklistSellingPoint(callToAction, title, items) {
+            this.type = 'checklist';
+            this.callToAction = callToAction;
+            
+            this.checklist = {
+                title : title,
+                items: items,
+            }
+        }
+        
         $scope.content = {};
         $scope.content.sellingPoints = [ 
             new QuotationSellingPoint(
-                'quotation',
                 'Learn more about our classrooms',
                 'Jack is always happy when I pick him up!',
                 '-Karen Smith',
                 'Mom to 4-month old Jack'
             ),
             new QuotationSellingPoint(
-                'quotation',
                 'Learn about our learning environment',
                 'Olivia already knows the periodic table, and she\'s only four!',
                 '-Will Edwards',
                 'Dad to 4-year old Olivia'
             ),
+            new ChecklistSellingPoint(
+                'See our menu',
+                'Our food is:',
+                ['organic', 'fresh', 'homemade']
+            ),
+            new ChecklistSellingPoint(
+                'Meet the staff of ACJS',
+                'Meet the owner Linda',
+                ['Always available', 'Commited to ACJS', 'Passionate']
+            ),            
         ];
     }
 ]);
