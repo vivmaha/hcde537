94447421aeee8f82dd4a902b6cb31ea08d12f72d
Vivek Maharajh <vmaha@ntdev.corp.microsoft.com>
2016-05-25 14:24:37 -0700

    added footer

--- src/modules/footer/footer.js
+++ src/modules/footer/footer.js
@@ -2,34 +2,6 @@ directives.directive('tltFooter', [
     function () {
         return {
             controller: function ($scope) {
-                $scope.items = [ { 
-                        title: "our school", 
-                        link: "/#/our-school" 
-                    }, { 
-                        title: "experience education", 
-                        link: "/#/experience-education" 
-                    }, { 
-                        title: "contact", 
-                        link: "/#/contact" 
-                    }, { 
-                        title: "current families", 
-                        link: "/#/current-families" 
-                    }, {
-                        title: "schedule tour",
-                        link: "/#/our-school/enroll/schedule-visit" 
-                    }, { 
-                        title: "facebook", 
-                        link: "https://www.facebook.com/childsjourney" 
-                    }, { 
-                        title: "yelp", 
-                        link: "http://www.yelp.com/biz/a-childs-journey-school-kirkland" 
-                    }, { 
-                        title: "sitemap", 
-                        link: "/#/sitemap" 
-                    }, { 
-                        title: "login", 
-                        link: "/#/current-families" 
-                    }, ];
             },
             replace: true,
             restrict: 'E',
