1c87ef08ce9142f9640638c5e5d585523f00ada2
Vivek Maharajh <vmaha@ntdev.corp.microsoft.com>
2016-05-31 08:22:14 -0700

    replaced placeholder text on homepage

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -7,26 +7,26 @@
         };
         $scope.blades = [ {
            heading: 'What Makes us Different',
-           paragraph: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint',
+           paragraph: 'All children embark on their own educational journey with enormous potential, which is nurtured through personal experience. We believe each journey, during these formative years, is more nourishing if it takes place within a warm, deeply-connected, and diverse community.',
            image: 'images/pages-Homepage-blade-1.png',
            callToAction: {
-               text: 'some button label',
+               text: 'our signature style',
                link: '/#/home/what-makes-us-different',
            },
         }, {
            heading: 'Our commitment to  learning and growing',
-           paragraph: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint',
+           paragraph: 'The intimate size of our community enables each child to be known, valued, and guided as an individual. Teachers interact with children individually, are familiar with their development, and appreciate their differences.',
            image: 'images/pages-Homepage-blade-2.png',
            callToAction: {
-               text: 'some button label',
+               text: 'learn more',
                link: '/#/home/our-commitment-to-learning-and-growing',
            },
         }, {
            heading: 'why families love us',
-           paragraph: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint',
+           paragraph: '“Great school. The staff is absolutely wonderful and all genuinely seem to enjoy doing what they do. My daughter loves going to this school and is absolutely thriving thanks to the excellent curriculum and skilled staff. Two thumbs up from me based on our experience!” - Melanie E.',
            image: 'images/pages-Homepage-blade-3.png',
            callToAction: {
-               text: 'some button label',
+               text: 'see more reviews',
                link: '/#/home/why-families-love-us',
            },
         }, {
@@ -34,7 +34,7 @@
            paragraph: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint',
            image: 'images/pages-Homepage-blade-4.png',
            callToAction: {
-               text: 'some button label',
+               text: 'learn more',
                link: '/#/home/fast-facts-for-new-families',
            },
         }, ];
